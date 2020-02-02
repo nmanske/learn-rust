f636479b3092450031b12611cc0805336889fb1b
Nathan Manske <nathan.manske@utexas.edu>
2020-02-02 12:21:08 -0600

    Complete minigrep by adding case sensitive env var and print errors to std error stream

--- RPL-Textbook/minigrep/src/lib.rs
+++ RPL-Textbook/minigrep/src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,15 +16,26 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -34,7 +47,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "dust";
         let contents = "\
 Things that rhyme with Rust:
@@ -42,10 +55,19 @@ dust
 must
 crust
 gust";
-        assert_eq!(
-            vec!["dust"],
-            search(query, contents)
-        );
+        assert_eq!(vec!["dust"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "dUsT";
+        let contents = "\
+Things that rhyme with Rust:
+dust
+must
+crust
+gust";
+        assert_eq!(vec!["dust"], search_case_insensitive(query, contents));
     }
 }
 
@@ -54,6 +76,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
